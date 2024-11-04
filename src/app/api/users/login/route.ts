9a595c2679f99897c7ce989674864c5ca881deef
wadile-hitesh <hiteshwadile.dev@gmail.com>
2024-11-04 09:54:53 +0530

    New Update

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -2,57 +2,65 @@ import { connect } from "@/config/dbConfig";
 import User from "@/models/userModels";
 import { NextRequest, NextResponse } from "next/server";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 
 connect();
 
 export async function POST(request: NextRequest) {
-    try {
-        
-        const reqBody = await request.json();
+  try {
+    const reqBody = await request.json();
 
-        const {email, password} = reqBody;
+    const { email, password } = reqBody;
 
-        if (!email || !password) {
-            return NextResponse.json({ error: "Please fill all fields" }, { status: 400 });
-        }
-
-        const user =await User.findOne({ email })
-
-        if (!user) {
-            return NextResponse.json({ error: "User does not exist" }, { status: 400 });
-        }
-
-        const validPassword = await bcrypt.compare(password, user.password);
-
-        if(!validPassword) {
-            return NextResponse.json({ error: "Invalid password" }, { status: 400 });
-        }
-
-        const tokenData = {
-            id : user._id,
-            username : user.username,
-            email : user.email
-        }
-
-        const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
-          expiresIn: "1d",
-        });
-
-        const response = NextResponse.json({
-            message: "Login successful",
-            success: true,
-        })
-
-        response.cookies.set("token",token, {
-            httpOnly: true,
-        })
-
-        return response;
-
-
-    } catch (error: any) {
-
-        return NextResponse.json({ error: error.message }, { status: 500 });
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Please fill all fields" },
+        { status: 400 }
+      );
     }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "User does not exist" },
+        { status: 400 }
+      );
+    }
+
+    const validPassword = await bcrypt.compare(password, user.password);
+
+    if (!validPassword) {
+      return NextResponse.json({ error: "Invalid password" }, { status: 400 });
+    }
+
+    const tokenData = {
+      id: user._id,
+      username: user.username,
+      email: user.email,
+    };
+
+    const token = await jwt.sign(tokenData, process.env.TOKEN_SECRET!, {
+      expiresIn: "1d",
+    });
+
+    const response = NextResponse.json({
+      message: "Login successful",
+      success: true,
+    });
+
+    response.cookies.set("token", token, {
+      httpOnly: true,
+    });
+
+    return response;
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return NextResponse.json({ error: error.message }, { status: 500 });
+    }
+    return NextResponse.json(
+      { error: "An unexpected error occurred" },
+      { status: 500 }
+    );
+  }
 }
